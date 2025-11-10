474d3b59ebfdd83c39ee00bdf89838e88f0059f0
USER <USER@DESKTOP-K0V9S8M>
2025-11-10 10:39:55 +0200

    Add project files.

---
+++ lab11/lab11.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+   
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+    int a, b, c, n, i;
+
+    cout << "Послідовність №3 (a[n+2] = a[n+1] + a[n])" << endl;
+    cout << "Введіть кількість чисел n = ";
+    cin >> n;
+
+    a = 1;
+    b = 2;
+
+    cout << a << endl;
+    cout << b << endl;
+
+    for (i = 3; i <= n; i++)
+    {
+        c = a + b;
+        cout << c << endl;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
